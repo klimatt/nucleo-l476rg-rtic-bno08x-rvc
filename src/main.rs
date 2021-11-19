9db02d9fe2ce294b2c365a2135d8e09dec8791cf
Matvei <matvei@wheel.me>
2021-11-19 10:30:56 +0100

    example for v0.1.2

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
 #![no_main]
 #![no_std]
 
-use rtt_target::{rprintln};
-use stm32l4xx_hal;
-use rtic::app;
-use cortex_m_rt::{exception, ExceptionFrame};
 use core::panic::PanicInfo;
 use core::sync::atomic;
 use core::sync::atomic::Ordering;
+use cortex_m_rt::{exception, ExceptionFrame};
+use rtic::app;
+use rtt_target::rprintln;
+use stm32l4xx_hal;
 
 pub const CLOCKS_FREQ_HZ: u32 = 80_000_000; //80 MHz
-pub type UsrLed = stm32l4xx_hal::gpio::gpioa::PA5<stm32l4xx_hal::gpio::Output<stm32l4xx_hal::gpio::PushPull>>;
+pub type UsrLed =
+    stm32l4xx_hal::gpio::gpioa::PA5<stm32l4xx_hal::gpio::Output<stm32l4xx_hal::gpio::PushPull>>;
 
 #[app(device = stm32l4xx_hal::stm32, peripherals = true, dispatchers = [TSC, FLASH])]
 mod app {
+    use crate::{rprintln, CLOCKS_FREQ_HZ};
+    use bbqueue::BBBuffer;
+    use bno08x_rvc;
+    use core::borrow::Borrow;
+    use cortex_m::asm;
     use dwt_systick_monotonic::DwtSystick;
     use rtt_target::rtt_init_print;
-    use crate::{rprintln, CLOCKS_FREQ_HZ};
     use stm32l4xx_hal::{
-        gpio::{GpioExt},
-        pac::{USART2},
+        gpio::GpioExt,
+        pac::USART2,
+        prelude::*,
         serial::{self, Config, Serial},
-        prelude::*
     };
-    use bno08x_rvc;
-    use cortex_m::asm;
-    use bbqueue::BBBuffer;
-    use core::borrow::Borrow;
 
     #[monotonic(binds = SysTick, default = true)]
     type MyMono = DwtSystick<{ CLOCKS_FREQ_HZ }>; // 1000 Hz / 1 ms granularity
@@ -42,10 +43,8 @@ mod app {
         pars: bno08x_rvc::parser::Parser,
     }
 
-
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
-
         static BB: BBBuffer<{ bno08x_rvc::BUFFER_SIZE }> = BBBuffer::new();
         rtt_init_print!();
         rprintln!("Initializing... ");
@@ -75,7 +74,9 @@ mod app {
             .pclk2(CLOCKS_FREQ_HZ.hz())
             .freeze(&mut flash.acr, &mut pwr);
 
-        let usr_led = gpioa.pa5.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+        let usr_led = gpioa
+            .pa5
+            .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
 
         let tx_pin = gpioa.pa2.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
         let rx_pin = gpioa.pa3.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
@@ -92,30 +93,32 @@ mod app {
         let (_tx, rx) = serial.split();
 
         let systick = cp.SYST;
-        let mut dcb =  cp.DCB;
-        let  dwt =  cp.DWT;
+        let mut dcb = cp.DCB;
+        let dwt = cp.DWT;
 
         let mono = DwtSystick::new(&mut dcb, dwt, systick, clocks.sysclk().0);
         rprintln!("done.");
 
         (
             Shared { pars },
-            Local {
-                usr_led,
-                proc,
-                rx
-            },
-            init::Monotonics(mono)
+            Local { usr_led, proc, rx },
+            init::Monotonics(mono),
         )
     }
 
-    #[idle(shared = [pars])]
-    fn idle(mut cx: idle::Context) -> !{
-        loop{
+    #[idle(shared = [pars], local = [usr_led, t: bool = true])]
+    fn idle(mut cx: idle::Context) -> ! {
+        loop {
             cx.shared.pars.lock(|p| {
-                rprintln!("Get last Frame: {:?}", p.get_last_raw_frame());
-
+                rprintln!("Get last Raw Frame: {:?}", p.get_last_raw_frame());
             });
+            if *cx.local.t {
+                cx.local.usr_led.set_high().ok();
+                *cx.local.t = false;
+            } else {
+                cx.local.usr_led.set_low().ok();
+                *cx.local.t = true;
+            }
             asm::delay(80_000_000);
         }
     }
@@ -123,11 +126,11 @@ mod app {
     #[task(shared = [pars], priority = 4)]
     fn parse(mut cx: parse::Context) {
         cx.shared.pars.lock(|p| {
-            match p.worker(|frame|{
-                rprintln!("Rx Frame: {:?}", frame);
-            }){
-                Ok(_) => { }
-                Err(_e) => { }
+            match p.worker(|frame| {
+                rprintln!("Rx Pretty Frame: {:?}", frame.as_pretty_frame());
+            }) {
+                Ok(_) => {}
+                Err(_e) => {}
             };
         })
     }
@@ -138,13 +141,13 @@ mod app {
         let proc = cx.local.proc;
 
         match rx.read() {
-            Ok(b) => {
-                match proc.process_slice(&[b]) {
-                    Ok(_) => {  parse::spawn(); }
-                    Err(_e) => {  }
+            Ok(b) => match proc.process_slice(&[b]) {
+                Ok(_) => {
+                    parse::spawn().unwrap();
                 }
+                Err(_e) => {}
             },
-            Err(_e) => { }
+            Err(_e) => {}
         };
     }
 }
